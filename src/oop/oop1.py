6f42ae672ea0e1c810a4376531223144ae67733a
Luis Mendes <cvlopes88@gmail.com>
2020-02-14 13:07:38 -0500

    oop1

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -1,5 +1,4 @@
 # Write classes for the following class hierarchy:
-#
 #  [Vehicle]->[FlightVehicle]->[Starship]
 #      |                |
 #      v                v
@@ -17,3 +16,27 @@
 #     pass
 #
 # Put a comment noting which class is the base class
+
+# setting up main class here
+class Vehicle:
+  def __init__(self, name, typeOfVehicle):
+    
+    self.name = name
+    self.typeOfVehicle = typeOfVehicle
+    
+    
+  def whatVehicle(self):
+    return f'{self.name} is a {self.typeOfVehicle}'
+    
+
+car = Vehicle('Car', 'GroundVehicle')
+print(car.whatVehicle())
+
+motorcycle = Vehicle('Motorcycle', 'GroundVehicle')
+print(motorcycle.whatVehicle())
+
+airplane = Vehicle('Airplane', 'FlightVehicle')
+print(airplane.whatVehicle())
+
+starship = Vehicle('Starship', 'FlightVehicle i think :)')
+print(starship.whatVehicle())
