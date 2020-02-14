4ab5b4c2c699576a6e03810dd0c41f75db17925a
Luis Mendes <cvlopes88@gmail.com>
2020-02-14 18:32:54 -0500

    all done

--- src/comp/comp.py
+++ src/comp/comp.py
@@ -41,7 +41,7 @@ print(b)
 # Write a list comprehension that creates a list of names of everyone
 # whose name starts with any letter between 'C' and 'G' inclusive.
 print("Starts between C and G, inclusive:")
-c = [i.name for i in humans if i.name[0] in ['C', 'G']]
+c = [i.name for i in humans if i.name[0] in ['C', 'D', 'E', 'F','G']]
 print(c)
 
 # Write a list comprehension that creates a list of all the ages plus 10.
@@ -59,7 +59,7 @@ print(e)
 # age, for example ("David", 31), for everyone between the ages of 27 and 32,
 # inclusive.
 print("Names and ages between 27 and 32:")
-f = [(i.name, i.age) for i in humans if i.age > 27 and i.age < 32]
+f = [(i.name, i.age) for i in humans if i.age >= 27 and i.age <= 32]
 print(f)
 
 # Write a list comprehension that creates a list of new Humans like the old
